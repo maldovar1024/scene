16daa30606cfcb23c3065b5b697f367fb11db67e
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2020-11-21 15:13:20 +0800

    feat: 添加绘图函数

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { mat4 } from 'gl-matrix';
 import {
   initIndicesBuffer,
   initTextureCoordinatesBuffer,
@@ -9,6 +10,7 @@ import './index.less';
 import fsSource from './shaders/fragment.frag';
 import vsSource from './shaders/vertex.vert';
 import { initShaderProgram, loadTexture } from './utils';
+import { enableVertexAttribArray } from './utils/enable-vertex-attribute';
 
 const canvas = document.getElementById('container') as HTMLCanvasElement;
 
@@ -21,21 +23,27 @@ if (!gl) {
 const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
 
 // 获取 attribute 的位置
-const vertexPosition = gl.getAttribLocation(shaderProgram, 'aVertexPosition');
-const vertexNormal = gl.getAttribLocation(shaderProgram, 'aVertexNormal');
-const textureCoord = gl.getAttribLocation(shaderProgram, 'aTextureCoord');
+const vertexPositionAttrib = gl.getAttribLocation(
+  shaderProgram,
+  'aVertexPosition'
+);
+const vertexNormalAttrib = gl.getAttribLocation(shaderProgram, 'aVertexNormal');
+const textureCoordAttrib = gl.getAttribLocation(shaderProgram, 'aTextureCoord');
 
 // 获取 uniform 的位置
-const projectionMatrix = gl.getUniformLocation(
+const projectionMatrixUniform = gl.getUniformLocation(
   shaderProgram,
   'uProjectionMatrix'
 );
-const modelViewMatrix = gl.getUniformLocation(
+const modelViewMatrixUniform = gl.getUniformLocation(
   shaderProgram,
   'uModelViewMatrix'
 );
-const normalMatrix = gl.getUniformLocation(shaderProgram, 'uNormalMatrix');
-const uSampler = gl.getUniformLocation(shaderProgram, 'uSampler');
+const normalMatrixUniform = gl.getUniformLocation(
+  shaderProgram,
+  'uNormalMatrix'
+);
+const uSamplerUniform = gl.getUniformLocation(shaderProgram, 'uSampler');
 
 // 创建缓冲区
 const positionBuffer = initVerticesBuffer(gl);
@@ -44,3 +52,87 @@ const textureCoordBuffer = initTextureCoordinatesBuffer(gl);
 const indicesBuffer = initIndicesBuffer(gl);
 
 const texture = loadTexture(gl, textureImg);
+
+let rotation = 0;
+let then = 0;
+
+function render(now: number) {
+  now *= 0.001;
+  const deltaTime = now - then;
+  then = now;
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  drawScene(gl!, rotation);
+  rotation += deltaTime;
+  window.requestAnimationFrame(render);
+}
+
+window.requestAnimationFrame(render);
+
+function drawScene(gl: WebGLRenderingContext, rotation: number) {
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clearDepth(1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.enable(gl.DEPTH_TEST);
+  gl.depthFunc(gl.LEQUAL);
+
+  // 创建透视矩阵
+  const projectionMatrix = mat4.create();
+  mat4.perspective(
+    projectionMatrix,
+    Math.PI / 4,
+    gl.canvas.width / gl.canvas.height,
+    0.1,
+    100
+  );
+
+  // 创建模型矩阵
+  const modelViewMatrix = mat4.create();
+  mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, 0.0, -6.0]);
+  mat4.rotate(modelViewMatrix, modelViewMatrix, rotation, [0, 0, 1]);
+  mat4.rotate(modelViewMatrix, modelViewMatrix, rotation * 0.7, [0, 1, 0]);
+
+  const normalMatrix = mat4.create();
+  mat4.invert(normalMatrix, modelViewMatrix);
+  mat4.transpose(normalMatrix, normalMatrix);
+
+  // 设置 attribute 变量
+  enableVertexAttribArray(gl, {
+    target: gl.ARRAY_BUFFER,
+    buffer: positionBuffer,
+    index: vertexPositionAttrib,
+    size: 3,
+    type: gl.FLOAT,
+  });
+  enableVertexAttribArray(gl, {
+    target: gl.ARRAY_BUFFER,
+    buffer: textureCoordBuffer,
+    index: textureCoordAttrib,
+    size: 2,
+    type: gl.FLOAT,
+  });
+  enableVertexAttribArray(gl, {
+    target: gl.ARRAY_BUFFER,
+    buffer: normalBuffer,
+    index: vertexNormalAttrib,
+    size: 3,
+    type: gl.FLOAT,
+  });
+
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesBuffer);
+
+  gl.useProgram(shaderProgram);
+
+  // 设置 uniform 变量
+  gl.uniformMatrix4fv(projectionMatrixUniform, false, projectionMatrix);
+  gl.uniformMatrix4fv(modelViewMatrixUniform, false, modelViewMatrix);
+  gl.uniformMatrix4fv(normalMatrixUniform, false, normalMatrix);
+
+  // TEXTURE0 表示注册到第一个纹理（支持同时使用 32 个纹理）
+  gl.activeTexture(gl.TEXTURE0);
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  // 使用 0 号纹理
+  gl.uniform1i(uSamplerUniform, 0);
+
+  // 绘制三角形
+  gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
+}
