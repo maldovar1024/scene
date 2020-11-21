9c92d3761ef594b8dc97921f3294de0a57323a31
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2020-11-21 13:03:55 +0800

    feat: 添加类型声明

---
+++ src/types.d.ts
@@ -0,0 +1,14 @@
+declare module '*.vert' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.frag' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const url: string;
+  export default url;
+}
